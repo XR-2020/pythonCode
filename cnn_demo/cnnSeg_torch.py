0de4b34e8f286ab054b98b5011b8f82bd905e17f
XR-2020 <1131585249@qq.com>
2022-07-28 11:59:16 +0800

    layer新增注释

--- cnn_demo/cnnSeg_torch.py
+++ cnn_demo/cnnSeg_torch.py
@@ -85,79 +85,6 @@ class up_conv(nn.Module):
         x = self.up(x)
         return x
 
-"""
-class U_Net(nn.Module):
-    # 输入是3个通道的RGB图，输出是0或1——因为我的任务是2分类任务
-    def __init__(self, in_ch=3, out_ch=2):
-        super(U_Net, self).__init__()
-        # 卷积参数设置
-        n1 = 64
-        filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
-        # 最大池化层
-        self.Maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.Maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.Maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.Maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        # 左边特征提取卷积层
-        self.Conv1 = conv_block(in_ch, filters[0])
-        self.Conv2 = conv_block(filters[0], filters[1])
-        self.Conv3 = conv_block(filters[1], filters[2])
-        self.Conv4 = conv_block(filters[2], filters[3])
-        self.Conv5 = conv_block(filters[3], filters[4])
-
-        # 右边特征融合反卷积层
-        self.Up5 = up_conv(filters[4], filters[3])
-        self.Up_conv5 = conv_block(filters[4], filters[3])
-
-        self.Up4 = up_conv(filters[3], filters[2])
-        self.Up_conv4 = conv_block(filters[3], filters[2])
-
-        self.Up3 = up_conv(filters[2], filters[1])
-        self.Up_conv3 = conv_block(filters[2], filters[1])
-
-        self.Up2 = up_conv(filters[1], filters[0])
-        self.Up_conv2 = conv_block(filters[1], filters[0])
-
-        self.Conv = nn.Conv2d(filters[0], out_ch, kernel_size=1, stride=1, padding=0)
-
-    # 前向计算，输出一张与原图相同尺寸的图片矩阵
-    def forward(self, x):
-        e1 = self.Conv1(x)
-
-        e2 = self.Maxpool1(e1)
-        e2 = self.Conv2(e2)
-
-        e3 = self.Maxpool2(e2)
-        e3 = self.Conv3(e3)
-
-        e4 = self.Maxpool3(e3)
-        e4 = self.Conv4(e4)
-
-        e5 = self.Maxpool4(e4)
-        e5 = self.Conv5(e5)
-
-        d5 = self.Up5(e5)
-        d5 = torch.cat((e4, d5), dim=1)  # 将e4特征图与d5特征图横向拼接
-
-        d5 = self.Up_conv5(d5)
-
-        d4 = self.Up4(d5)
-        d4 = torch.cat((e3, d4), dim=1)  # 将e3特征图与d4特征图横向拼接
-        d4 = self.Up_conv4(d4)
-
-        d3 = self.Up3(d4)
-        d3 = torch.cat((e2, d3), dim=1)  # 将e2特征图与d3特征图横向拼接
-        d3 = self.Up_conv3(d3)
-
-        d2 = self.Up2(d3)
-        d2 = torch.cat((e1, d2), dim=1)  # 将e1特征图与d1特征图横向拼接
-        d2 = self.Up_conv2(d2)
-
-        out = nn.Sigmoid()(self.Conv(d2))
-        return out
-"""
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -215,9 +142,23 @@ class Net(nn.Module):
         return out,list
 
     def conv_sequential(self, in_size, out_size, kfilter, padding, kernel_size, stride):
+        """
+        Args:
+            in_size: 输入通道个数
+            out_size: 输出通道个数
+            kfilter: 卷积时卷积核大小
+            padding: 填充
+            kernel_size:池化时卷积核大小
+            stride: 步长
+
+        Returns:
+
+        """
         return nn.Sequential(
             nn.Conv2d(in_size, out_size, kfilter, padding=padding),
             nn.BatchNorm2d(out_size),
+            #1.当inplace=True的时候，会改变输入数据；当inplace=False的时候，不会改变输入数据，
+            #2.激活函数：ReLU（）将小于0的值置为0，大于0 的保存下来
             nn.ReLU(True),
             nn.MaxPool2d(kernel_size, stride)
         )
